ebe4f7bb1f060d09d1b76a1809b75649bd26bf24
Benjamin Wasty <benny.wasty@gmail.com>
2019-07-27 23:38:15 +0200

    copy-paste gltf example

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,224 @@
 use amethyst::{
-    core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
+    animation::{
+        get_animation_set, AnimationBundle, AnimationCommand, AnimationControlSet, AnimationSet,
+        EndControl, VertexSkinningBundle,
+    },
+    assets::{
+        AssetPrefab, Completion, Handle, Prefab, PrefabData, PrefabLoader, PrefabLoaderSystem,
+        ProgressCounter, RonFormat,
+    },
+    controls::{ControlTagPrefab, FlyControlBundle},
+    core::transform::{Transform, TransformBundle},
+    derive::PrefabData,
+    ecs::{Entity, ReadStorage, Write, WriteStorage},
+    input::{is_close_requested, is_key_down, StringBindings, VirtualKeyCode},
     prelude::*,
     renderer::{
-        pass::DrawShadedDesc,
-        rendy::{
-            factory::Factory,
-            graph::{
-                render::{RenderGroupDesc, SubpassBuilder},
-                GraphBuilder,
-            },
-            hal::{format::Format, image},
-        },
+        camera::CameraPrefab,
+        light::LightPrefab,
+        plugins::{RenderPbr3D, RenderSkybox, RenderToWindow},
         types::DefaultBackend,
-        GraphCreator, RenderingSystem,
+        RenderingBundle,
     },
-    utils::application_root_dir,
-    window::{ScreenDimensions, Window, WindowBundle},
+    utils::{
+        application_root_dir,
+        auto_fov::{AutoFov, AutoFovSystem},
+        tag::{Tag, TagFinder},
+    },
+    Error,
 };
+use amethyst::gltf::{GltfSceneAsset, GltfSceneFormat, GltfSceneLoaderSystem};
 
-struct MyState;
+use serde::{Deserialize, Serialize};
 
-impl SimpleState for MyState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+#[derive(Default)]
+struct Example {
+    entity: Option<Entity>,
+    initialised: bool,
+    progress: Option<ProgressCounter>,
 }
 
-fn main() -> amethyst::Result<()> {
+#[derive(Clone, Serialize, Deserialize)]
+struct AnimationMarker;
+
+#[derive(Default)]
+struct Scene {
+    handle: Option<Handle<Prefab<ScenePrefabData>>>,
+    animation_index: usize,
+}
+
+#[derive(Default, Deserialize, Serialize, PrefabData)]
+#[serde(default)]
+struct ScenePrefabData {
+    transform: Option<Transform>,
+    gltf: Option<AssetPrefab<GltfSceneAsset, GltfSceneFormat>>,
+    camera: Option<CameraPrefab>,
+    auto_fov: Option<AutoFov>,
+    light: Option<LightPrefab>,
+    tag: Option<Tag<AnimationMarker>>,
+    fly_tag: Option<ControlTagPrefab>,
+}
+
+impl SimpleState for Example {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let StateData { world, .. } = data;
+
+        self.progress = Some(ProgressCounter::default());
+
+        world.exec(
+            |(loader, mut scene): (PrefabLoader<'_, ScenePrefabData>, Write<'_, Scene>)| {
+                scene.handle = Some(loader.load(
+                    "prefab/puffy_scene.ron",
+                    RonFormat,
+                    self.progress.as_mut().unwrap(),
+                ));
+            },
+        );
+    }
+
+    fn handle_event(
+        &mut self,
+        data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        let StateData { world, .. } = data;
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                Trans::Quit
+            } else if is_key_down(&event, VirtualKeyCode::Space) {
+                toggle_or_cycle_animation(
+                    self.entity,
+                    &mut world.write_resource(),
+                    &world.read_storage(),
+                    &mut world.write_storage(),
+                );
+                Trans::None
+            } else {
+                Trans::None
+            }
+        } else {
+            Trans::None
+        }
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if !self.initialised {
+            let remove = match self.progress.as_ref().map(|p| p.complete()) {
+                None | Some(Completion::Loading) => false,
+
+                Some(Completion::Complete) => {
+                    let scene_handle = data
+                        .world
+                        .read_resource::<Scene>()
+                        .handle
+                        .as_ref()
+                        .unwrap()
+                        .clone();
+
+                    data.world.create_entity().with(scene_handle).build();
+
+                    true
+                }
+
+                Some(Completion::Failed) => {
+                    println!("Error: {:?}", self.progress.as_ref().unwrap().errors());
+                    return Trans::Quit;
+                }
+            };
+            if remove {
+                self.progress = None;
+            }
+            if self.entity.is_none() {
+                if let Some(entity) = data
+                    .world
+                    .exec(|finder: TagFinder<'_, AnimationMarker>| finder.find())
+                {
+                    self.entity = Some(entity);
+                    self.initialised = true;
+                }
+            }
+        }
+        Trans::None
+    }
+}
+
+fn toggle_or_cycle_animation(
+    entity: Option<Entity>,
+    scene: &mut Scene,
+    sets: &ReadStorage<'_, AnimationSet<usize, Transform>>,
+    controls: &mut WriteStorage<'_, AnimationControlSet<usize, Transform>>,
+) {
+    if let Some((entity, Some(animations))) = entity.map(|entity| (entity, sets.get(entity))) {
+        if animations.animations.len() > scene.animation_index {
+            let animation = animations.animations.get(&scene.animation_index).unwrap();
+            let set = get_animation_set::<usize, Transform>(controls, entity).unwrap();
+            if set.has_animation(scene.animation_index) {
+                set.toggle(scene.animation_index);
+            } else {
+                println!("Running animation {}", scene.animation_index);
+                set.add_animation(
+                    scene.animation_index,
+                    animation,
+                    EndControl::Normal,
+                    1.0,
+                    AnimationCommand::Start,
+                );
+            }
+            scene.animation_index += 1;
+            if scene.animation_index >= animations.animations.len() {
+                scene.animation_index = 0;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), amethyst::Error> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
 
-    let resources_dir = app_root.join("resources");
-    let display_config_path = resources_dir.join("display_config.ron");
+    let display_config_path = app_root.join("resources/display_config.ron");
+    let assets_directory = app_root.join("assets/");
 
     let game_data = GameDataBuilder::default()
-        .with_bundle(WindowBundle::from_config_path(display_config_path))?
-        .with_bundle(TransformBundle::new())?
-        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
-            RenderingGraph::default(),
-        ));
+        .with(AutoFovSystem::default(), "auto_fov", &[])
+        .with(
+            PrefabLoaderSystem::<ScenePrefabData>::default(),
+            "scene_loader",
+            &[],
+        )
+        .with(
+            GltfSceneLoaderSystem::default(),
+            "gltf_loader",
+            &["scene_loader"], // This is important so that entity instantiation is performed in a single frame.
+        )
+        .with_bundle(
+            AnimationBundle::<usize, Transform>::new("animation_control", "sampler_interpolation")
+                .with_dep(&["gltf_loader"]),
+        )?
+        .with_bundle(
+            FlyControlBundle::<StringBindings>::new(None, None, None)
+                .with_sensitivity(0.1, 0.1)
+                .with_speed(5.),
+        )?
+        .with_bundle(TransformBundle::new().with_dep(&[
+            "animation_control",
+            "sampler_interpolation",
+            "fly_movement",
+        ]))?
+        .with_bundle(VertexSkinningBundle::new().with_dep(&[
+            "transform_system",
+            "animation_control",
+            "sampler_interpolation",
+        ]))?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(RenderToWindow::from_config_path(display_config_path))
+                .with_plugin(RenderPbr3D::default().with_skinning())
+                .with_plugin(RenderSkybox::default()),
+        )?;
 
-    let mut game = Application::new(resources_dir, MyState, game_data)?;
+    let mut game = Application::build(assets_directory, Example::default())?.build(game_data)?;
     game.run();
-
     Ok(())
-}
-
-#[derive(Default)]
-struct RenderingGraph {
-    dimensions: Option<ScreenDimensions>,
-    surface_format: Option<Format>,
-    dirty: bool,
-}
-
-impl GraphCreator<DefaultBackend> for RenderingGraph {
-    fn rebuild(&mut self, res: &Resources) -> bool {
-        // Rebuild when dimensions change, but wait until at least two frames have the same.
-        let new_dimensions = res.try_fetch::<ScreenDimensions>();
-        use std::ops::Deref;
-        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
-            self.dirty = true;
-            self.dimensions = new_dimensions.map(|d| d.clone());
-            return false;
-        }
-
-        self.dirty
-    }
-
-    fn builder(
-        &mut self,
-        factory: &mut Factory<DefaultBackend>,
-        res: &Resources,
-    ) -> GraphBuilder<DefaultBackend, Resources> {
-        use amethyst::renderer::rendy::{
-            graph::present::PresentNode,
-            hal::command::{ClearDepthStencil, ClearValue},
-        };
-
-        self.dirty = false;
-        let window = <ReadExpect<'_, Window>>::fetch(res);
-        let surface = factory.create_surface(&window);
-        // cache surface format to speed things up
-        let surface_format = *self
-            .surface_format
-            .get_or_insert_with(|| factory.get_surface_format(&surface));
-        let dimensions = self.dimensions.as_ref().unwrap();
-        let window_kind =
-            image::Kind::D2(dimensions.width() as u32, dimensions.height() as u32, 1, 1);
-
-        let mut graph_builder = GraphBuilder::new();
-        let color = graph_builder.create_image(
-            window_kind,
-            1,
-            surface_format,
-            Some(ClearValue::Color([0.34, 0.36, 0.52, 1.0].into())),
-        );
-
-        let depth = graph_builder.create_image(
-            window_kind,
-            1,
-            Format::D32Sfloat,
-            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
-        );
-
-        let opaque = graph_builder.add_node(
-            SubpassBuilder::new()
-                .with_group(DrawShadedDesc::new().builder())
-                .with_color(color)
-                .with_depth_stencil(depth)
-                .into_pass(),
-        );
-
-        let _present = graph_builder
-            .add_node(PresentNode::builder(factory, surface, color).with_dependency(opaque));
-
-        graph_builder
-    }
 }
